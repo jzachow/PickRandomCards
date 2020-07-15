6980d55ca1da35abfe7240a7a99e386b1d56f133
Justin <justin.zachow@gmail.com>
2020-07-14 21:30:55 -0400

    Create CardPicker class and define main method

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Dynamic;
+
 
 namespace PickRandomCards
 {
@@ -6,7 +8,23 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter the number of cards you wish to draw:");
+            string userInput = Console.ReadLine();
+            int numberOfCards;  
+
+            while(!int.TryParse(userInput, out numberOfCards) || numberOfCards < 0)
+            {
+                Console.WriteLine("Invalid input. Please try again");
+                Console.WriteLine("Please enter the number of cards you wish to draw:");
+                userInput = Console.ReadLine();
+            }
+
+            string[] hand = CardPicker.PickSomeCards(numberOfCards);
+
+            foreach (string card in hand)
+            {
+                Console.WriteLine(card);
+            }
         }
     }
 }
